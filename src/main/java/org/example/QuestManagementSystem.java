e2a3334398efcdc0de1febcf8f7630a4e9c4c9f0
DenizYalim <116675869+DenizYalim@users.noreply.github.com>
2025-03-19 00:52:45 +0300

    semi implemented main

--- src/main/java/org/example/QuestManagementSystem.java
+++ src/main/java/org/example/QuestManagementSystem.java
@@ -4,6 +4,14 @@ public class QuestManagementSystem
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Adventurer adv1 = new Adventurer("Frodo", Adventurer.Races.Human);
+        Adventurer adv2 = new Adventurer("Aragon", Adventurer.Races.Human);
+        Adventurer adv3 = new Adventurer("Lego", Adventurer.Races.Elf);
+        Adventurer adv4 = new Adventurer("Gibli", Adventurer.Races.Human);
+
+        Quest q1 = new Quest();
+        Quest q2 = new Quest();
+
+        Mentor mentor = new Mentor("Gandalf", "the Grey");
     }
 }
