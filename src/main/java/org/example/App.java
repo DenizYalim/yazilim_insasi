dabd954987408fa88a5c9fa939262441416fe482
DenizYalim <116675869+DenizYalim@users.noreply.github.com>
2025-05-14 15:12:10 +0000

    todo

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
-package org.example;
+public static void main(String[] args) {
+Car car1 = new LuxuryCar("BMW", "7 Series", "35ABC123");
+car1 = new GPSDecorator(car1);
+car1 = new LeatherSeatsDecorator(car1);
 
-/**
- * Hello world!
- */
+RentalRecord record1 = new RentalRecord(car1, 3, "John Doe");
+record1.printInvoice();
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+System.out.println();
+
+Car car2 = new EconomicCar("Toyota", "Yaris", "06XYZ987");
+car2 = new HybridDecorator(car2);
+
+RentalRecord record2 = new RentalRecord(car2, 5, "Jane Smith");
+record2.printInvoice();
+
 }
