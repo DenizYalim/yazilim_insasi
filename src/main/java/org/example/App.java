99ee2c4dc0e5eec7080cde66f37bd356e47712f9
DenizYalim <116675869+DenizYalim@users.noreply.github.com>
2025-05-14 15:17:13 +0000

    ye

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,17 +1,5 @@
-public static void main(String[] args) {
-Car car1 = new LuxuryCar("BMW", "7 Series", "35ABC123");
-car1 = new GPSDecorator(car1);
-car1 = new LeatherSeatsDecorator(car1);
+package org.example;
 
-RentalRecord record1 = new RentalRecord(car1, 3, "John Doe");
-record1.printInvoice();
-
-System.out.println();
-
-Car car2 = new EconomicCar("Toyota", "Yaris", "06XYZ987");
-car2 = new HybridDecorator(car2);
-
-RentalRecord record2 = new RentalRecord(car2, 5, "Jane Smith");
-record2.printInvoice();
+public class App{
 
 }
